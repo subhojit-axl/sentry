2c05558a1b6acfaeca6c5e60fdf61f9cc826b73b
Subhojit Paul <subhojit.paul@axelerant.com>
2022-01-14 13:38:00 +0530

    change config location

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -768,7 +768,7 @@ $settings['migrate_node_migrate_type_classic'] = FALSE;
 if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
   include $app_root . '/' . $site_path . '/settings.local.php';
 }
-$settings['config_sync_directory'] = './config';
+$settings['config_sync_directory'] = './../config/sync';
 
 // Automatic Platform.sh settings.
 if (file_exists($app_root . '/' . $site_path . '/settings.platformsh.php')) {
